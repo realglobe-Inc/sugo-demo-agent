b113d111ae38b92eb1213bacf87f83cc70a52229
Taka Okunishi <okunishitaka.com@gmail.com>
2016-06-27 15:58:32 +0900

    Version incremented to 1.0.4

--- example/example-usage.js
+++ example/example-usage.js
@@ -4,11 +4,11 @@ const sugoDemoAgent = require('sugo-demo-agent')
 const co = require('co')
 
 co(function * () {
-  let agent = sugoDemoAgent('http://example.com')
+  let agent = sugoDemoAgent('http://my-sever.com')
 
+  // Check if server available
   {
-    // Check if server available
-    let ok = yield agent.knock()
+    let ok = yield agent.knock() // Send HTTP HEAD request.
     /** ... */
   }
 }).catch((err) => console.error(err))
