7bc18384998af10a16b3cfabf7b0c8ed0cc9117b
Taka Okunishi <okunishitaka.com@gmail.com>
2016-06-27 15:42:06 +0900

    Add tests

--- test/sugo_demo_agent_test.js
+++ test/sugo_demo_agent_test.js
@@ -4,23 +4,38 @@
  */
 'use strict'
 
-const sugoDemoAgent = require('../lib/sugo_demo_agent.js')
+const SugoDemoAgent = require('../lib/sugo_demo_agent.js')
 const assert = require('assert')
+const sgServer = require('sg-server')
+const { freeport } = sgServer
 const co = require('co')
 
 describe('sugo-demo-agent', function () {
   this.timeout(3000)
-
+  let server, port
   before(() => co(function * () {
+    port = yield freeport()
+    server = sgServer({
+      endpoints: {
+        '/api/foo': (ctx) => {
+          ctx.set('foo', 'This is foo')
+          ctx.body = null
+        }
+      }
+    })
 
+    yield server.listen(port)
   }))
 
   after(() => co(function * () {
-
+    yield server.close()
   }))
 
   it('Sugo demo agent', () => co(function * () {
-
+    let agent = new SugoDemoAgent(`http://localhost:${port}`)
+    assert.ok(agent)
+    let ok = yield agent.knock('/api/foo')
+    assert.ok(ok)
   }))
 })
 
